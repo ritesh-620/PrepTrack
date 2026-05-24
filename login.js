8117032b49f429570a8e41674a7e33f0d030ebc5
Ritesh kumar ray <riteshroy62095@gmail.com>
2026-05-25 00:30:20 +0530

    update my dashboard with new changes and added the login page

---
+++ login.js
@@ -0,0 +1,14 @@
+function login() {
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
+
+  if(user == "Ritesh@62" && pass=="62095") {
+    window.location.href = "home.html";
+  } else {
+    alert("Wrong user or password. Please try again.");
+  }
+}
+
+function Guest() {
+  window.location.href = "home.html";
+}
